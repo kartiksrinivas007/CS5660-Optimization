4ebcc40b54c279765c56d0f63961ae9a105d4063
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2024-04-07 10:37:29 +0530

    Add Custom Intializations

---
+++ init.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch
+
+
+
+def custom_weight_init(m, device):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+    pass
