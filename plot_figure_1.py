cc1f5fb1705a7adb2704d66b694178c6a6ec4f2e
Dishank422 <dishankjain0112358@gmail.com>
2024-04-08 18:07:39 +0530

    file to be run

--- plot_figure_1.py
+++ plot_figure_1.py
@@ -1,13 +1,12 @@
 from main import main
 from utils.test import test
 from utils.parser import argument_parser
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 import matplotlib.pyplot as plt
 
 num_experiments = 5
 
-def gradient():
-    accs=[]
+def gradient(accs):
     for i in range(num_experiments):
         args = argument_parser()
         model, test_loader = main(args)
@@ -15,8 +14,7 @@ def gradient():
         accs.append(test(model, test_loader, args))
     print(f"SGD: {accs}")
 
-def mirror_descent(q):
-    accs=[]
+def mirror_descent(q, accs):
     for i in range(num_experiments):
         args = argument_parser()
         args.algorithm = "smd"
@@ -27,12 +25,20 @@ def mirror_descent(q):
     print(f"SMD q={q}: {accs}")
 
 if __name__ == "__main__":
-    p1 = Process(target=gradient)
-    p2 = Process(target=mirror_descent, args=(1.01,))
-    p3 = Process(target=mirror_descent, args=(3,))
-    p4 = Process(target=mirror_descent, args=(8,))
-    p5 = Process(target=mirror_descent, args=(10,))
-    p6 = Process(target=mirror_descent, args=(14,))
+    manager = Manager()
+    smd1 = manager.list()
+    smd3 = manager.list()
+    smd8 = manager.list()
+    smd10 = manager.list()
+    smd14 = manager.list()
+    sgd = manager.list()
+
+    p1 = Process(target=gradient, args=(sgd,))
+    p2 = Process(target=mirror_descent, args=(1.01, smd1,))
+    p3 = Process(target=mirror_descent, args=(3, smd3,))
+    p4 = Process(target=mirror_descent, args=(8, smd8,))
+    p5 = Process(target=mirror_descent, args=(10, smd10,))
+    p6 = Process(target=mirror_descent, args=(14, smd14,))
 
     p1.start()
     p2.start()
@@ -49,10 +55,10 @@ if __name__ == "__main__":
     p6.join()
 
     # create box plot of accuracies
-    # plt.boxplot([smd1, sgd, smd3, smd8, smd10, smd14], labels=["SMD q=1", "SGD", "SMD q=3", "SMD q=8", "SMD q=10", "SMD q=14"])
-    # plt.ylabel("Accuracy")
-    # plt.xlabel("Algorithm")
-    # plt.legend()
-    # plt.title("Comparison of SGD and SMD")
-    # plt.savefig("figure_1.png")
-    # plt.show()
+    plt.boxplot([smd1, sgd, smd3, smd8, smd10, smd14], labels=["SMD q=1", "SGD", "SMD q=3", "SMD q=8", "SMD q=10", "SMD q=14"])
+    plt.ylabel("Accuracy")
+    plt.xlabel("Algorithm")
+    plt.legend()
+    plt.title("Comparison of SGD and SMD")
+    plt.savefig("figure_1.png")
+    plt.show()
