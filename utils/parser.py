02ecdffdcee5af86d82576443f8ba5a29ba1d120
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2024-04-11 18:42:34 +0530

    Regularization hyperparameter setting to 1e-3

--- utils/parser.py
+++ utils/parser.py
@@ -9,7 +9,7 @@ def argument_parser():
     parser.add_argument('--lr', type=float, default=0.01, help='The learning rate')
     parser.add_argument('--device', type=str, default="cuda", help='The device to run on')
     parser.add_argument('--q_norm', type=float, default=2, help='The q norm')
-    parser.add_argument('--reg', type=float, default=0.01, help='The regularization parameter for the one-norm')
+    parser.add_argument('--reg', type=float, default=0.001, help='The regularization parameter for the one-norm')
     args = parser.parse_args()
     return args
 
