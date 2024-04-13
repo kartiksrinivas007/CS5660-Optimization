4e4c49d1134bddafff2b20034466f82eccd7b935
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2024-04-14 00:11:03 +0530

    Add arg for MAGICO

--- Dataset/data.py
+++ Dataset/data.py
@@ -5,6 +5,7 @@ import torch.utils.data as tud
 import torch.utils.data.dataset as dataset
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from .MAGIC import *
 
 def return_loader(args):
     if (args.dataset == "cifar10"):
@@ -18,9 +19,18 @@ def return_loader(args):
         cifar10_testloader = tud.DataLoader(cifar10_trainset, batch_size = args.batch_size, shuffle=False, num_workers=2)
         cifar10_valset, cifar10_testset = train_test_split(cifar10_testset, test_size=0.5)
         cifar10_valoader = tud.DataLoader(cifar10_valset, batch_size = args.batch_size, shuffle=False, num_workers=2)
-        # breakpoint()
+
         return cifar10_trainloader, cifar10_testloader, cifar10_valoader
-    
+    if (args.dataset == "magic04s"):
+        # note that this dataset by default is normalized even the test set!
+        
+        magic04s_X, magic04s_y = magic04s()
+        magic04s_train_X, magic04s_test_X, magic04s_val_X, magic04s_train_y, magic04s_test_y, magic04s_val_y = split_data(magic04s_X, magic04s_y, args)
+        magic04s_trainloader = tud.DataLoader(dataset.TensorDataset(torch.tensor(magic04s_train_X), torch.tensor(magic04s_train_y)), batch_size=args.batch_size, shuffle=True)
+        magic04s_testloader = tud.DataLoader(dataset.TensorDataset(torch.tensor(magic04s_test_X), torch.tensor(magic04s_test_y)), batch_size=args.batch_size, shuffle=False)
+        magic04s_valoader = tud.DataLoader(dataset.TensorDataset(torch.tensor(magic04s_val_X), torch.tensor(magic04s_val_y)), batch_size=args.batch_size, shuffle=False)
+        return magic04s_trainloader, magic04s_testloader, magic04s_valoader
+
     assert False, f"{args.dset} is not a valid dataset"
 
 def test():
