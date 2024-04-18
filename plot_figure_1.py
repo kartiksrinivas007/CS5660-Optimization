2dbfe87f58f4ab93da62b0aef165269e6583345d
Dishank422 <dishankjain0112358@gmail.com>
2024-04-18 10:19:34 +0530

    gpu parallelism for plot_figure_1.py

--- plot_figure_1.py
+++ plot_figure_1.py
@@ -8,27 +8,30 @@ num_experiments = 5
 num_epochs = 4500
 batch_size = 128
 
-def gradient(accs):
+def gradient(accs, device):
     for i in range(num_experiments):
         args = argument_parser()
         args.num_epochs = num_epochs
         args.batch_size = batch_size
+        args.device = "cuda:"+str(device)
         model, test_loader = main(args)
         accs.append(test(model, test_loader, args))
     print(f"SGD: {accs}")
 
-def mirror_descent(q, accs):
+def mirror_descent(q, accs, device):
     for i in range(num_experiments):
         args = argument_parser()
         args.algorithm = "smd"
         args.q_norm = q
         args.batch_size = batch_size
         args.num_epochs = num_epochs
+        args.device = "cuda:" + str(device)
         model, test_loader = main(args)
         accs.append(test(model, test_loader, args))
     print(f"SMD q={q}: {accs}")
 
 if __name__ == "__main__":
+    d1, d2, d3, d4, d5, d6 = 0, 0, 0, 0, 0, 0
     manager = Manager()
     smd1 = manager.list()
     smd3 = manager.list()
@@ -37,12 +40,12 @@ if __name__ == "__main__":
     smd14 = manager.list()
     sgd = manager.list()
 
-    p1 = Process(target=gradient, args=(sgd,))
-    p2 = Process(target=mirror_descent, args=(1.1, smd1,))
-    p3 = Process(target=mirror_descent, args=(3, smd3,))
-    p4 = Process(target=mirror_descent, args=(8, smd8,))
-    p5 = Process(target=mirror_descent, args=(10, smd10,))
-    p6 = Process(target=mirror_descent, args=(14, smd14,))
+    p1 = Process(target=gradient, args=(sgd,d1,))
+    p2 = Process(target=mirror_descent, args=(1.1, smd1, d2,))
+    p3 = Process(target=mirror_descent, args=(3, smd3, d3,))
+    p4 = Process(target=mirror_descent, args=(8, smd8, d4,))
+    p5 = Process(target=mirror_descent, args=(10, smd10, d5,))
+    p6 = Process(target=mirror_descent, args=(14, smd14, d6,))
 
     p1.start()
     p2.start()
