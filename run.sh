006014e83dc00cc7c8851db98e12ad7075cafa17
Dishank422 <dishankjain0112358@gmail.com>
2024-04-18 17:39:50 +0530

    tried everything i could. nothing works

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ python3 main.py --q_norm 10.0 --batch_size 128 --lr 0.0001 --num_epochs 20 --reg
 
 python3 main.py --q_norm 10.0 --batch_size 128 --lr 0.0001 --num_epochs 20 --reg 1e-4 --algorithm=accelerated_prox_smd
 
-# For MAGIC datasets, lambda=1e-3 and num_epochs=1000 as per https://dl.acm.org/doi/pdf/10.1145/1553374.1553493
-# Learning rate was 0.001 in code but that lead to explosion.
+# For MAGIC datasets, lr=0.001, lambda=1e-3 and num_epochs=1000 as per https://dl.acm.org/doi/pdf/10.1145/1553374.1553493
+# I got above from their code
 python3 main.py --dataset magic04s --model linear --lr 0.0001 --num_epochs 200 --q_norm 10.0 --reg 1e-3
 
 python3 main.py --dataset magic04s --model linear --lr 0.000001 --num_epochs 200 --q_norm 10.0 --reg 1e-3 --algorithm=smd
